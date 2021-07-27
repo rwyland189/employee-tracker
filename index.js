5e6aefbeeff9b2f6466d01c1cb474b6d9d43b6ea
Becca Wyland <rwyland@uwalumni.com>
2021-07-26 21:43:36 -0500

    write startApp() that will use inquirer to prompt the user on what they'd like to do

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
+// require inquirer
+const inquirer = require('inquirer');
+
 // require the connection.js
 const db = require('./db/connection');
+
+// start the application
+startApp();
+
+function startApp() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "option",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Exit"
+            ]
+        }
+    ]).then(function (answer) {
+        if (answer.option === 'View all departments') {
+            viewAllDepartments();
+        } else if (answer.option === 'View all roles') {
+            viewAllRoles();
+        } else if (answer.option === 'View all employees') {
+            viewAllEmployees();
+        } else if (answer.option === 'Exit') {
+            db.end();
+        }
+    })
+}
 
 // functions needed
 
@@ -8,11 +40,3 @@ const db = require('./db/connection');
 // view all roles
 
 // view all employees
-
-// add a department
-
-// add a role
-
-// add an employee
-
-// update an employee
